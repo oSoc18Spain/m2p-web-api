f1041bfb378e5609944df8ad59b449ed0c7c60f1
rajila <rajila@espol.edu.ec>
2018-07-18 12:34:18 +0200

    Cambio error Task

--- src/main/java/com/m2p/web/api/models/services/TaskService.java
+++ src/main/java/com/m2p/web/api/models/services/TaskService.java
@@ -3,11 +3,13 @@ package com.m2p.web.api.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.m2p.web.api.models.dao.ITaskDao;
 import com.m2p.web.api.models.entity.Task;
 
+@Service
 public class TaskService implements ITaskService {
 	@Autowired
 	private ITaskDao taskDao;
